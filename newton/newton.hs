d6311013d2465747728609db31be695ba452f688
emptyTuple <kotorov@outlook.com>
2023-11-08 23:10:24 +0700

    newton

--- newton/newton.hs
+++ newton/newton.hs
@@ -9,5 +9,19 @@ module Newton where
 newton :: (t -> Bool) -> (t -> t) -> t -> t
 newton isGood improve = start where
     start x | isGood x = x
-            | otherwise = start (improve x)
+            | otherwise = start $ improve x
 
+-- Let find ⁿ√x. The function is f(x) = xⁿ - a and the task is to solve xⁿ - a = 0
+
+root :: (Integral p, Num t, Ord t) => p -> (t -> t -> t) -> t -> t -> t
+root n improveRoot eps a = newton isGoodRoot (improveRoot a) 1 where
+    isGoodRoot x = abs (x ^ n - a) < eps
+
+eps :: Double
+eps = 0.00000000000000001
+
+sqrt' :: Double -> Double
+sqrt' = root 2 (\a x -> (x + a / x) / 2) eps
+
+cbrt :: Double -> Double
+cbrt = root 3 (\a x -> (2 * x + a / x ^ 2) / 3) eps
